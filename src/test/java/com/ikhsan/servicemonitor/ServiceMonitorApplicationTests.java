cdf07ed7c97ff2feaf29aee202bd066369e64070
ikhsanmaaa <ikhsanmaaa@gmail.com>
2026-05-07 12:31:41 +0700

    init springboot project

---
+++ src/test/java/com/ikhsan/servicemonitor/ServiceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikhsan.servicemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
